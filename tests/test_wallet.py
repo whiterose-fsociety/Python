a49241bcc88fa891dfe783c3207d9bb6d86596a1
Elliot Alderson <iammrrobot24@gmail.com>
2022-07-23 21:57:26 +0200

    Continuous Integration

---
+++ tests/test_wallet.py
@@ -0,0 +1,19 @@
+import pytest
+import sys 
+sys.path.append("/home/molefe/Playground/Testing/Python/project/wallet")
+from wallet import Wallet,InsufficientAmount
+
+
+@pytest.fixture
+def my_wallet():
+    """Returns a Wallet Instance With A Zero Balance"""
+    return Wallet()
+
+@pytest.mark.parametrize("earned,spent,expected",[
+(30, 10, 20),
+(20, 2, 18),
+])
+def test_transaction(my_wallet,earned,spent,expected):
+    my_wallet.add_cash(earned)
+    my_wallet.spend_cash(spent)
+    assert my_wallet.balance == expected
